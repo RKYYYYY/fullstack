af952fbc2436b1caf0b3f9e010918e6d3b442131
dylanmnr <reaky@outlook.fr>
2025-08-19 13:44:21 +0200

    exemple input réutilisable

--- src/components/Common/Button.jsx
+++ src/components/Common/Button.jsx
@@ -2,7 +2,7 @@ import { variants } from "../../constants";
 
 export default function Button({ color, text }) {
   return (
-    <button className={`w-3xl px-4 py-2 rounded-2xl ${variants[color]}`}>
+    <button className={`w-[200px] px-4 py-2 rounded-2xl ${variants[color]}`}>
       {text}
     </button>
   );
