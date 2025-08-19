c74b1311799604cf91679ea5ac3f76ca5b8674cf
dylanmnr <reaky@outlook.fr>
2025-08-19 11:32:37 +0200

    importation de l'header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,36 @@
-export default function Header(){
-    return(
-        <div>
-            <p>
-                Header
-            </p>
-        </div>
-    );
+import { NavLink } from "react-router-dom";
+
+export default function Header({ userConnected }) {
+  return (
+    <header className="bg-white shadow-md p-4 flex flex-row justify-between items-center">
+      <NavLink to="/">
+        <span className="text-xl font-bold text-blue-500">BLOG 3000</span>
+      </NavLink>
+      {userConnected ? (
+        <nav className="flex space-x-6 items-center">
+          <a href="#" className="text-gray-600 hover:text-black font-semibold">
+            Déconnexion
+          </a>
+          <a href="#" className="text-gray-600 hover:text-black font-semibold">
+            Profil
+          </a>
+        </nav>
+      ) : (
+        <nav className="flex space-x-6">
+          <NavLink
+            to="/login"
+            className="text-gray-600 hover:text-black font-semibold"
+          >
+            Connexion
+          </NavLink>
+          <NavLink
+            to="/register"
+            className="text-gray-600 hover:text-black font-semibold"
+          >
+            Inscription
+          </NavLink>
+        </nav>
+      )}
+    </header>
+  );
 }
